3c2db83672bce43a547328ce07e9ab2ded23706b
faranzki <franziska.meinherz@epfl.ch>
2017-08-08 13:51:44 -0500

    mavlink conversion repo added

---
+++ catkin_ws/src/mavlink_conversion/src/converter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <mavros_msgs/mavlink_convert.h> //requires C++ 11, which needs to be activated in the CMakeLists.txt file in the /src directory of the package
+#include <mavconn/mavlink_dialect.h>
+
+int main(int argc, char **argv){
+
+	std::cout << "test" << std::endl;
+
+	return 0;
+}
