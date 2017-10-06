473f6931552aa5762aa12ec1879c7cc11dc9935f
ROS <ros@ros-indigo-desktop-trusty64>
2017-10-06 13:30:30 +0000

    removing buildfiles and adding gitignore

--- catkin_ws/build/data_extraction/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/viki/data_treatment/catkin_ws/src/data_extraction
-# Build directory: /home/viki/data_treatment/catkin_ws/build/data_extraction
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
