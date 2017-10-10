54246740ff31a00eeb35f380932538f91d8b3ea6
Beat <beat.geissmann@gmx.ch>
2017-10-10 21:47:03 +0200

    Laser data and mavlink msg extraction updated

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/beat/Downloads/Octanis1-ROS-master/data_treatment/catkin_ws/src
-# Build directory: /home/beat/Downloads/Octanis1-ROS-master/data_treatment/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(stereocamera_export)
-subdirs(data_extraction)
--- catkin_ws/build/data_extraction/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/beat/Downloads/Octanis1-ROS-master/data_treatment/catkin_ws/src/data_extraction
-# Build directory: /home/beat/Downloads/Octanis1-ROS-master/data_treatment/catkin_ws/build/data_extraction
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
